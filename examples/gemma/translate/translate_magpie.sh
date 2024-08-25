36e108e8d9f5997ed904de68c318323bf56d776a
kazuki <kaz.tokyo.tech20@gmail.com>
2024-08-25 15:02:35 +0900

    feat: rename files

--- examples/gemma/translate/translate_magpie.sh
+++ examples/gemma/translate/translate_magpie.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #$ -cwd
 #$ -l node_q=1
-#$ -l h_rt=24:00:00
+#$ -l h_rt=72:00:00
 #$ -o outputs/inference/$JOB_ID.log
 #$ -e outputs/inference/$JOB_ID.log
 #$ -p -5
@@ -24,14 +24,14 @@ UNIFIED_CKPT_PATH=/gs/bs/tga-NII-LLM/checkpoints/tensorRT/unified/gemma-2-$varia
 ENGINE_PATH=/gs/bs/tga-NII-LLM/checkpoints/tensorRT/engine/gemma2/$variant/bf16/1-gpu/
 VOCAB_FILE_PATH=/gs/bs/tga-NII-LLM/hf-checkpoints/gemma-2-$variant-it/tokenizer.model
 
-DATASET_DIR=/gs/bs/tga-NII-LLM/datasets/raw/instruct/OpenOrca
+DATASET_DIR=/gs/bs/tga-NII-LLM/datasets/raw/instruct/synthetic/magpie-ultra-v0.1/data
 
-DATASET_PATH=${DATASET_DIR}/split_1M_1M-GPT4-Augmented_60.jsonl
-OUTPUT_PATH=${DATASET_DIR}/lm_translated_1M-GPT4-Augmented_60.jsonl
+DATASET_PATH=${DATASET_DIR}/split_train-00001-of-00002_3.jsonl
+OUTPUT_PATH=${DATASET_DIR}/lm_translated_train-00001-of-00002_3.jsonl
 
 echo "DATASET_PATH: $DATASET_PATH"
 
-python examples/gemma/batch_translate.py \
+python examples/gemma/translate/translate_magpie.py \
   --jsonl-path $DATASET_PATH \
   --output-path $OUTPUT_PATH \
   --vocab_file ${VOCAB_FILE_PATH} \
