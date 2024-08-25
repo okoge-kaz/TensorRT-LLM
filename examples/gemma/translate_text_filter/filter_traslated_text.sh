c6d361fe75c8aa4d6fd98b1523e2649de51e166a
kazuki <kaz.tokyo.tech20@gmail.com>
2024-08-25 15:01:15 +0900

    chore: rename files

--- examples/gemma/translate_text_filter/filter_traslated_text.sh
+++ examples/gemma/translate_text_filter/filter_traslated_text.sh
@@ -7,7 +7,7 @@ DATASET_DIR=/gs/bs/tga-NII-LLM/datasets/raw/instruct/OpenOrca/
 
 for INDEX in $(seq $START_INDEX $END_INDEX); do
   echo "Running job for index: $INDEX"
-  python examples/gemma/preprocess_translated_text.py \
+  python examples/gemma/translate_text_filter/filter_translated_text.py \
     --input ${DATASET_DIR}/lm_translated_1M-GPT4-Augmented_${INDEX}.jsonl \
     --output ${DATASET_DIR}/lm_translated_${INDEX}.jsonl
 done
