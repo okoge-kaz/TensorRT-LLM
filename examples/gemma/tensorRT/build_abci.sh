cc6ef31cf5b64c0fdcc778977b3e9ca230df594b
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-04 23:21:26 +0900

    feat: abci

--- examples/gemma/tensorRT/build_abci.sh
+++ examples/gemma/tensorRT/build_abci.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=0:12:00:00
+#$ -j y
+#$ -o outputs/gemma/build/
+#$ -cwd
 
 set -e
 
@@ -16,8 +21,8 @@ source .env/bin/activate
 variant=27b
 
 CKPT_PATH=/groups/gag51395/hf-checkpoints/gemma-2-$variant-it/
-UNIFIED_CKPT_PATH=/bb/llm/gaf51275/checkpoints/tensorRT/unified/gemma-2-$variant-it/bf16/tp2/
-ENGINE_PATH=/bb/llm/gaf51275/checkpoints/tensorRT/engine/gemma2/$variant/bf16/2-gpu/
+UNIFIED_CKPT_PATH=/bb/llm/gaf51275/checkpoints/tensorRT/unified/gemma-2-$variant-it/bf16/tp8/
+ENGINE_PATH=/bb/llm/gaf51275/checkpoints/tensorRT/engine/gemma2/$variant/bf16/8-gpu/
 VOCAB_FILE_PATH=/groups/gag51395/hf-checkpoints/gemma-2-$variant-it/tokenizer.model
 
 mkdir -p ${UNIFIED_CKPT_PATH}
@@ -27,12 +32,12 @@ python ./examples/gemma/convert_checkpoint.py \
   --ckpt-type hf \
   --model-dir ${CKPT_PATH} \
   --dtype bfloat16 \
-  --world-size 2 \
+  --world-size 8 \
   --output-model-dir ${UNIFIED_CKPT_PATH}
 
 trtllm-build --checkpoint_dir ${UNIFIED_CKPT_PATH} \
   --gemm_plugin auto \
-  --max_batch_size 1 \
+  --max_batch_size 16 \
   --max_input_len 3000 \
   --max_seq_len 4096 \
   --lookup_plugin bfloat16 \
